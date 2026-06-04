f6ec5b56b3b8a4494f77e8cc3d44ca57fc572935
davidpfann <pfann.david@gmail.com>
2026-06-04 23:14:18 +0200

    Update people-counter.py

--- people-counter.py
+++ people-counter.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 st.set_page_config(layout="centered")
 
-# --- 1. NAČTENÍ SEZNAMU (Musí být na začátku, aby o něm Python věděl) ---
-@st.cache_data(ttl=60)  # Kešuje seznam na 1 minutu
+# --- 1. NAČTENÍ SEZNAMU ---
+@st.cache_data(ttl=60)
 def nacti_scitace():
     default_list = ["– (Vyber jméno)", "Sčítač 1", "Sčítač 2"]
     try:
@@ -36,12 +36,12 @@ def reset_formulkare():
 if "mod_dopravy_key" not in st.session_state:
     reset_formulkare()
 
-# --- ROZHRANÍ APLIKACE (MAXIMÁLNĚ KOMPAKTNÍ) ---
+# --- ROZHRANÍ APLIKACE ---
 
-# Mód pohybu
+# Mód pohybu (Opraven překlep s HTML tagem)
 mod_dopravy = st.radio(
     "Mód pohybu", 
-    ["Chodec 🚶", "Běžec <b>🏃</b>", "Kolo 🚴", "Koloběžka 🛴", "Jiné"], 
+    ["Chodec 🚶", "Běžec 🏃", "Kolo 🚴", "Koloběžka 🛴", "Jiné"], 
     horizontal=True, 
     key="mod_dopravy_key",
     label_visibility="collapsed"
@@ -50,29 +50,26 @@ mod_dopravy = st.radio(
 # Skupina
 ve_skupine = st.checkbox("👥 Šel/šla ve skupině s předchozím", key="ve_skupine_key")
 
-# Třífázové parametry
-c1, c2, c3 = st.columns(3)
-with c1:
-    ma_psa = st.radio("Pes? 🐕", ["–", "Ano", "Ne"], horizontal=True, key="ma_psa_key")
-with c2:
-    ma_nakup = st.radio("Nákup? 🛍️", ["–", "Ano", "Ne"], horizontal=True, key="ma_nakup_key")
-with c3:
-    ma_aktovku = st.radio("Školní aktovka? 🎒", ["–", "Ano", "Ne"], horizontal=True, key="ma_aktovku_key")
+# --- PARAMETRY NA JEDEN ŘÁDEK ---
+# Použijeme sloupce, kde vlevo je text a vpravo tlačítka, vertikálně zarovnaná
 
-col_extra1, col_extra2 = st.columns(2)
-with col_extra1:
-    je_otocka = st.radio("Otočka/Návrat? 🔄", ["–", "Ano", "Ne"], horizontal=True, key="je_otocka_key")
-with col_extra2:
-    vek = st.selectbox("Věk", ["– (Nezadáno)", "Produktivní", "Dítě", "Teenager", "Senior"], key="vek_key", label_visibility="collapsed")
+# 1. Pes
+r1_col1, r1_col2 = st.columns([1, 2])
+with r1_col1:
+    st.markdown("<div style='padding-top: 5px;'><b>Pes? 🐕</b></div>", unsafe_html=True)
+with r1_col2:
+    ma_psa = st.radio("Pes", ["–", "Ano", "Ne"], horizontal=True, key="ma_psa_key", label_visibility="collapsed")
 
-# Poznámka
-poznamka = st.text_input("Poznámka", placeholder="Dobrovolná poznámka...", key="poznamka_key", label_visibility="collapsed")
+# 2. Nákup
+r2_col1, r2_col2 = st.columns([1, 2])
+with r2_col1:
+    st.markdown("<div style='padding-top: 5px;'><b>Nákup? 🛍️</b></div>", unsafe_html=True)
+with r2_col2:
+    ma_nakup = st.radio("Nákup", ["–", "Ano", "Ne"], horizontal=True, key="ma_nakup_key", label_visibility="collapsed")
 
-# --- VÝBĚR SČÍTAČE (Nyní bezpečně dole nad tlačítky) ---
-st.write("")
-scitac = st.selectbox("Jméno sčítače", seznam_scitacu)
-
-# --- LOGIKA ZÁPISU ---
-def zapis_zaznam(smer):
-    if scitac == "– (Vyber jméno)":
-        st.error
+# 3. Aktovka
+r3_col1, r3_col2 = st.columns([1, 2])
+with r3_col1:
+    st.markdown("<div style='padding-top: 5px;'><b>Aktovka? 🎒</b></div>", unsafe_html=True)
+with r3_col2:
+    ma_aktovku = st.radio("Aktovka", ["–", "Ano", "Ne"], horizontal=True, key="ma_aktovku_
